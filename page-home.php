b66e66b522e75054cc8bca4ae9c565e422c6c2dc
root <bhat.skanda.m@gmail.com>
2020-05-29 17:53:26 +0000

    Hompage layouting done

--- page-home.php
+++ page-home.php
@@ -4,50 +4,59 @@
  */
 
 get_header(); ?>
-  <div class="container">
-    <div class="row"></div>
+  <div class="position-relative overflow-hidden">
+    <div class="container">
       <section id="primary" class="content-area col-sm-12">
         <main id="main" class="site-main" role="main">
           <article>
-            <div class="ph-hero">
+            <div>
               <div class="heading">
                 <h1>Accelerate human-machine collaboration</h1>
               </div>
               <div class="subheading">
-                <p>Jiffy.ai’s revolutionary automation suite turbo-charges productivity, transforms processes, and helps your teams
-                  unleash
-                  their creativity and innovation.</p>
+                <p>Jiffy.ai’s revolutionary automation suite turbo-charges productivity, transforms processes, and helps
+                  your teams unleash their creativity and innovation.</p>
               </div>
-              <button class="btn-primary primary-cta">Request Demo</button>
+              <button class="btn-primary primary-cta">REQUEST DEMO</button>
             </div>
-
+    
             <div class="row justify-content-between main-card-row">
-              <div class="col-sm-12 col-md-3 main-card">
-                <h2>Jiffy.ai Automate</h2>
+              <div class="card main-card card-automate">
+                <img class="img" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/automate_icon.png" />
+                <h2>JIFFY.ai Automate</h2>
                 <p>Business processes, tranformed</p>
               </div>
-              <div class="col-sm-12 col-md-3 main-card">
-                <h2>Jiffy.ai Automate</h2>
-                <p>Business processes, tranformed</p>
+              <div class="card main-card card-assure">
+                <img class="img" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/assure_icon.png" />
+                <h2>JIFFY.ai Assure</h2>
+                <p>Test automation, perfected</p>
               </div>
-              <div class="col-sm-12 col-md-3 main-card">
-                <h2>Jiffy.ai Automate</h2>
-                <p>Business processes, tranformed</p>
+              <div class="card main-card card-innovate">
+                <img class="img" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/innovate_icon.png" />
+                <h2>JIFFY.ai Innovate</h2>
+                <p>Software development, reimagined</p>
               </div>
             </div>
-
+    
           </article>
         </main><!-- #main -->
       </section><!-- #primary -->
     </div>
+
+    <img class="img green-circle-1" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/home_green_circle.png" />
+    <img class="img ochre-square-1" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/home_ochre_square.png" />
+    <img class="img green-circle-2" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/home_green_circle.png" />
+
   </div>
+  
+
   <section class="hyper-apps bg-light">
     <div class="container">
       <div class="row justify-content-between">
         <div class="col-sm-12 col-md-6">
           <img src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/hyper_apps.png" />
         </div>
-        <div class="col-sm-12 col-md-5 apple">
+        <div class="col-sm-12 col-md-6">
           <h2>Meet Jiffy.ai’s Hyper Apps</h2>
           <h4>Our Hyper Apps outdo basic apps by easing the most complex business roadblocks, with surprising ease.</h4>
           <p>Jiffy.ai’s Invoice Processing Hyper App is the only comprehensive solution that addresses all the gaps and
@@ -57,7 +66,9 @@ get_header(); ?>
       </div>
     </div>
   </section>
-  <section class="use-cases">
+
+
+  <section class="use-cases position-relative overflow-hidden">
     <div class="container">
       <h4 class="text-center text-primary">Use Cases</h4>
       <div class="row">
@@ -69,59 +80,54 @@ get_header(); ?>
           <p>KYC processes are tedious, expensive and inaccurate. Jiffy-Automate’s result-focused and intuitive app takes over the
           monotonous tasks seamlessly. Not only are your documents read accurately, the app learns over time and improves itself,
           minimizing human intervention.</p>
-          <button class="btn-outline-primary">Read More</button>
+          <button class="btn-outline-primary">READ MORE</button>
         </div>
         <div class="col-sm-12 col-md-4">
           <div class="text-center">
-            <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/use_case_1.png" />
+            <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/use_case_2.png" />
           </div>         
-          <h6>KYC Process Automation with Intelligent RPA</h6>
-          <p>KYC processes are tedious, expensive and inaccurate. Jiffy-Automate’s result-focused and intuitive app takes over
-            the
-            monotonous tasks seamlessly. Not only are your documents read accurately, the app learns over time and improves
-            itself,
-            minimizing human intervention.</p>
-          <button class="btn-outline-primary">Read More</button>
+          <h6>Hyperautomating Group Airline Bookings</h6>
+          <p>Managing group travel bookings can be rather frustrating for the customer service teams managed by airlines. At the very least, each booking drains them by 3+ hours. Enter Jiffy-Automate. With a single smart bot, we can process more than 10,000 transactions, every single day.</p>
+          <button class="btn-outline-primary">READ MORE</button>
         </div>
         <div class="col-sm-12 col-md-4">
           <div class="text-center">
-            <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/use_case_1.png" />
+            <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/use_case_3.png" />
           </div>
-          <h6>KYC Process Automation with Intelligent RPA</h6>
-          <p>KYC processes are tedious, expensive and inaccurate. Jiffy-Automate’s result-focused and intuitive app takes over
-            the
-            monotonous tasks seamlessly. Not only are your documents read accurately, the app learns over time and improves
-            itself,
-            minimizing human intervention.</p>
-          <button class="btn-outline-primary">Read More</button>
+          <h6>Transforming Finance and Accounting Processes</h6>
+          <p>Companies that have implemented the Jiffy-Automate app for F&A report that they are able to realize returns on their investment in six months or less, with success rates as high as 85%, we’ve made this critical process more foolproof, nimble and cost-effective.</p>
+          <button class="btn-outline-primary">READ MORE</button>
         </div>
       </div>
     </div>
+    <img class="img green-circle-3" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/home_green_circle.png" />
   </section>
+
+
   <section class="partners bg-light">
     <div class="container">
       <h4 class="text-center text-secondary">Partners</h4>
       <div class="row justify-content-center">
         <div class="col-sm-12 col-md-10">
-          <div class="row">
+          <div class="row align-items-center">
             <div class="col-sm-12 col-md-3">
               <div class="text-center">
-                <img src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/hcl-1.png"/>
+                <img class="hcl" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/hcl.png"/>
               </div>
             </div>
             <div class="col-sm-12 col-md-3">
               <div class="text-center">
-                <img src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/hcl-1.png"/>
+                <img class="kpmg" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/kpmg.png"/>
               </div>
             </div>
             <div class="col-sm-12 col-md-3">
               <div class="text-center">
-                <img src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/hcl-1.png"/>
+                <img class="tech-m" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/tech_mahindra.png"/>
               </div>
             </div>
             <div class="col-sm-12 col-md-3">
               <div class="text-center">
-                <img src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/hcl-1.png"/>
+                <img class="deloitte" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/deloitte.png"/>
               </div>
             </div>
           </div>
@@ -129,44 +135,49 @@ get_header(); ?>
       </div>
     </div>
   </section>
-  <section class="case-studies">
+
+
+  <section class="case-studies position-relative overflow-hidden">
     <div class="container">
       <div class="row justify-content-between">
         <div class="col-sm-12 col-md-3">
           <h4 class="text-primary">Case Studies</h4>  
           <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h6>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-          <button class="btn-outline-primary">Read More</button>
+          <button class="btn-outline-primary">READ MORE</button>
         </div>
         <div class="col-sm-12 col-md-8">
           <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/case_studies.png" />
         </div>
       </div>
     </div>
+    <img class="img green-semi" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/home_green_semi.png" />
   </section>
+
+
   <section class="blog-list bg-light">
     <div class="container">
       <div class="row">
         <div class="col-sm-12 col-md-3">
-          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb.png" />
+          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb_1.png" />
           <div class="category text-secondary">CATEGORY</div>
           <h5>Lorem Ipsum is simply dummy text of the</h5>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
         </div>
         <div class="col-sm-12 col-md-3">
-          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb.png" />
+          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb_2.png" />
           <div class="category text-secondary">CATEGORY</div>
           <h5>Lorem Ipsum is simply dummy text of the</h5>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
         </div>
         <div class="col-sm-12 col-md-3">
-          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb.png" />
+          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb_3.png" />
           <div class="category text-secondary">CATEGORY</div>
           <h5>Lorem Ipsum is simply dummy text of the</h5>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
         </div>
         <div class="col-sm-12 col-md-3">
-          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb.png" />
+          <img class="img-fluid" src="<?php echo wp_get_upload_dir()['baseurl'] ?>/2020/05/blog_thumb_4.png" />
           <div class="category text-secondary">CATEGORY</div>
           <h5>Lorem Ipsum is simply dummy text of the</h5>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
