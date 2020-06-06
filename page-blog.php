c3fd8530043ed1af0d9b2211e273570bf010d125
root <bhat.skanda.m@gmail.com>
2020-06-06 11:17:52 +0000

    added aggregate pages

---
+++ page-blog.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* Template Name: Blog
+ */
+
+get_header(); ?>
+  <div class="position-relative hero">
+    <div class="container">
+      <section id="primary" class="content-area col-sm-12">
+        <main id="main" class="site-main" role="main">
+          <article>
+            <div>
+              <div class="heading">
+                <h1>Blog</h1>
+              </div>
+            </div>
+          </article>
+        </main><!-- #main -->
+      </section><!-- #primary -->
+    </div>
+  </div>
+
+
+  <section class="transform bg-light">
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-sm-12 col-md-12">
+        <?php
+        // the query
+        $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+        <?php if ( $wpb_all_query->have_posts() ) : ?>
+        <ul>
+            <!-- the loop -->
+            <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php endwhile; ?>
+            <!-- end of the loop -->
+        </ul>
+            <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php
+get_footer();
---
+++ page-casestudies.php
@@ -0,0 +1,59 @@
+<?php
+/**
+* Template Name: CaseStudies
+ */
+
+get_header(); ?>
+  <div class="position-relative hero">
+    <div class="container">
+      <section id="primary" class="content-area col-sm-12">
+        <main id="main" class="site-main" role="main">
+          <article>
+            <div>
+              <div class="heading">
+                <h1>Blog</h1>
+              </div>
+            </div>
+          </article>
+        </main><!-- #main -->
+      </section><!-- #primary -->
+    </div>
+  </div>
+
+
+  <section class="transform bg-light">
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-sm-12 col-md-12">
+        <?php
+        // the query
+        $args = array(
+            'post_type' => 'page',
+            'posts_per_page' => -1,
+            'meta_query' => array(
+                array(
+                    'key' => '_wp_page_template',
+                    'value' => 'page-case_study.php'
+                )
+            )
+        );
+        $wpb_all_query = new WP_Query($args); ?>
+        <?php if ( $wpb_all_query->have_posts() ) : ?>
+        <ul>
+            <!-- the loop -->
+            <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php endwhile; ?>
+            <!-- end of the loop -->
+        </ul>
+            <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php
+get_footer();
