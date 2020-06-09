54718f1e44ebc964d85404b46b340bd6cf656fa0
root <bhat.skanda.m@gmail.com>
2020-06-09 18:23:06 +0000

    fix security

--- functions.php
+++ functions.php
@@ -7,10 +7,6 @@
  * @package WP_Bootstrap_Starter
  */
 
-@ini_set( 'upload_max_size' , '64M' );
-@ini_set( 'post_max_size', '64M');
-@ini_set( 'max_execution_time', '300' );
-
 if ( ! function_exists( 'wp_bootstrap_starter_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
